5009ce7ef1f786c06bd9a5d8a08ce71ca5864acf
SushilPythonCode <91310468+SushilPythonCode@users.noreply.github.com>
2021-09-30 11:19:37 +0530

    Delete project directory

--- project/crud_api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import *
-
-class PersonSerializer(serializers.ModelSerializer):
-   class Meta:
-       model = Person
-       fields = ('id','name', 'birth_year', 'eye_color')
-
--- project/crud_api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from crud_api import views
-from .views import *
-urlpatterns = [
-   path('getlist/',Personlist.as_view()),
- 
-
-]
